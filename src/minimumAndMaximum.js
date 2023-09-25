223f86c0f100b122c4d83cbcf03d8c36a15f8d0f
Callie Giles <anchoredinthecrown@gmail.com>
2023-09-25 14:33:07 -0400

    update functions

--- src/minimumAndMaximum.js
+++ src/minimumAndMaximum.js
@@ -1,11 +1,39 @@
 /**
  * Implement an algorithm that sorts the array then returns the minimum and maximum
  */
-function minimumAndMaximumSort(numbers) {}
+function minimumAndMaximumSort(numbers) {
+    if (numbers.length === 0) {
+      return [];
+  }
+
+  numbers.sort(function(a, b) {
+      return a - b;
+  });
+
+  return [numbers[0], numbers[numbers.length - 1]];
+}
 
 /**
- * Implement an algorithm that uses iteration to find the minimum and maximum
- */
-function minimumAndMaximumIterate(numbers) {}
+* Implement an algorithm that uses iteration to find the minimum and maximum
+*/
+function minimumAndMaximumIterate(numbers) {
+if (numbers.length === 0) {
+      return [];
+  }
+
+  let minNum = numbers[0];
+  let maxNum = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] < minNum) {
+          minNum = numbers[i];
+      }
+      if (numbers[i] > maxNum) {
+          maxNum = numbers[i];
+      }
+  }
+
+  return [minNum, maxNum];
+}
 
 module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
--- src/missingNumber.js
+++ src/missingNumber.js
@@ -1,11 +1,23 @@
 /**
  * Implement a brute force algorithm for finding the missing number in an array
  */
-function missingNumberBruteForce(numbers) {}
+function missingNumberBruteForce(numbers) {
+    const n = numbers.length + 1;
+    const expectedSum = (n * (n + 1)) / 2;
+    const actualSum = numbers.reduce((acc, num) => acc + num, 0);
+    return expectedSum - actualSum;
+}
 
-/**
- * Use an iterative  strategy for finding the missing number in an array
- */
-function missingNumberSum(numbers) {}
+function missingNumberSum(numbers) {
+    let n = numbers.length + 1;
+    let expectedSum = (n * (n + 1)) / 2;
+    let actualSum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        actualSum += numbers[i];
+    }
+
+    return expectedSum - actualSum;
+}
 
 module.exports = { missingNumberBruteForce, missingNumberSum };
